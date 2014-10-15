cc485b782ab3b1a2b634191da30076e76b4d4253
eugene <eugene@gmail.com>
2014-10-14 17:42:53 -0700

    fixed module structure

--- SweetHome.py
+++ SweetHome.py
@@ -1,20 +1,26 @@
-from flask import Flask, make_response, render_template
+# from flask import Flask, make_response, render_template
 from flask.ext.script import Manager
 
+from app import create_app
 
-app = Flask(__name__)
+app = create_app()
+#
+# app = Flask(__name__)
+
 manager = Manager(app)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('ship_animation.html')
-
-
-@app.route('/ship')
-def ship():
-    return render_template('ship_animation.html')
-
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return render_template('ship_animation.html')
+#
+#
+# @app.route('/ship')
+# def ship():
+#     return render_template('ship_animation.html')
+#
 
 if __name__ == '__main__':
     manager.run()
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from app.life import life
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+bootstrap = None
+
+def create_app():
+    app = Flask(__name__)
+    bootstrap = Bootstrap(app)
+    app.register_blueprint(life.life)
+    return app
+
 
